e4e4e660269686c46cff74812d9c59534f73ed7e
Abhiroop Tejomay <abhirooptejomay@gmail.com>
2021-03-28 16:09:42 +0530

    added greedy algorithms

---
+++ algorithms/greedy_algorithms.py
@@ -0,0 +1,26 @@
+# the set covering problem
+if __name__ == "__main__":
+    states_needed = set(["mt", "wa", "or", "id", "nv", "ut",
+        "ca", "az"])
+
+    stations = {}
+    stations["kone"] = set(["id", "nv", "ut"])
+    stations["ktwo"] = set(["wa", "id", "mt"])
+    stations["kthree"] = set(["or", "nv", "ca"])
+    stations["kfour"] = set(["nv", "ut"])
+    stations["kfive"] = set(["ca", "az"])
+
+    final_stations = set()
+
+    while states_needed:
+        best_station = None 
+        states_covered = set() 
+        for station, states_for_station in stations.items():
+            covered = states_needed & states_for_station
+            if len(covered) > len(states_covered):
+                best_station = station 
+                states_covered = covered
+        final_stations.add(best_station)
+        states_needed -= states_covered
+
+    print(final_stations)
